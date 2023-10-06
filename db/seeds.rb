8ed54a9f38db0bced54b85b88e16b80882688fb5
SN_sumachan <bike.cbrider@gmail.com>
2023-10-06 18:10:21 +0900

    ビュー調整、seed仮置き

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!(
+  first_name: '管理者',
+  last_name: '太郎',
+  first_name_kana: 'カンリシャ',
+  last_name_kana: 'タロウ',
+  email: 'fff@gmail.com',
+  password: 'fff444',
+  password_confirmation: 'fff444',
+  admin: true
+)
+
+
