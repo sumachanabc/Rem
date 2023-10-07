24325238a4fd31a0c3776b657b327b9b4ba16ca9
SN_sumachan <bike.cbrider@gmail.com>
2023-10-07 16:19:20 +0900

    adminユーザー情報変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 User.create!(
-  first_name: '管理者',
-  last_name: '太郎',
-  first_name_kana: 'カンリシャ',
-  last_name_kana: 'タロウ',
+  first_name: '太郎',
+  last_name: '管理者',
+  first_name_kana: 'タロウ',
+  last_name_kana: 'カンリシャ',
   email: 'fff@gmail.com',
   password: 'fff444',
   password_confirmation: 'fff444',
