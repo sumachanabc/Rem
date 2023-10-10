903bee3fd9eb1b3839921e8442bbd438554da32a
SN_sumachan <bike.cbrider@gmail.com>
2023-10-10 23:58:14 +0900

    エラーメッセージ日本語化

--- app/models/condo.rb
+++ app/models/condo.rb
@@ -8,7 +8,7 @@ class Condo < ApplicationRecord
   # バリデーション
   with_options presence: true do
     validates :condo_name
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' } # 数字3桁、ハイフン、数字4桁の並びのみ許可する
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'は無効です。ハイフン（-）を含めてください' } # 数字3桁、ハイフン、数字4桁の並びのみ許可する
     validates :condo_address
     validates :floor, numericality: { only_integer: true, less_than_or_equal_to: 999 }
     validates :total_number_of_unit, numericality: { only_integer: true, less_than_or_equal_to: 9999 }
@@ -19,7 +19,7 @@ class Condo < ApplicationRecord
     validates :maintenance_fee
     validates :repair_reserve_fund
     validates :management_company_name
-    validates :management_company_postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' } # 数字3桁、ハイフン、数字4桁の並びのみ許可する
+    validates :management_company_postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'は無効です。ハイフン（-）を含めてください' } # 数字3桁、ハイフン、数字4桁の並びのみ許可する
     validates :management_company_address
     validates :management_company_phone_number, format: { with: /\A\d+\z/, message: 'は数字のみで入力してください' }
     validates :management_type_id, numericality: { other_than: 1 }
