a086ccfb32de981a0987baaf3759dba8cbd62b7e
SN_sumachan <bike.cbrider@gmail.com>
2023-10-30 23:44:11 +0900

    コントローラー記述

--- app/controllers/condo_user_posts_controller.rb
+++ app/controllers/condo_user_posts_controller.rb
@@ -6,8 +6,17 @@ class CondoUserPostsController < ApplicationController
   end
 
   def new
+    @condo_user_post = CondoUserPost.new
   end
 
   def create
+    CondoUserPost.create(condo_user_post_params)
+    redirect_to root_path
   end
+end
+
+private
+
+def condo_user_post_params
+  params.require(:condo_user_post).permit(:image, :text).merge(condo_user_id: current_condo_user.id)
 end
