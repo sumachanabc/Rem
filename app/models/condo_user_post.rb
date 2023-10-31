b06e63b5fac2c9d7ec0e96ef14831fef22385508
SN_sumachan <bike.cbrider@gmail.com>
2023-11-01 00:20:33 +0900

    Active Storage導入に伴う記述変更

--- app/models/condo_user_post.rb
+++ app/models/condo_user_post.rb
@@ -1,6 +1,7 @@
 class CondoUserPost < ApplicationRecord
   belongs_to :condo
   belongs_to :condo_user
+  has_one_attached :image
 
   validates :title, presence: true
   validates :content, presence: true
