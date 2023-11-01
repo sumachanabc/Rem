f571480facabb488728f7493cdc75b7617bb6729
SN_sumachan <bike.cbrider@gmail.com>
2023-11-02 03:16:13 +0900

    ビュー調整

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
       get 'inquiries'
     end
     resources :condo_users, only: [:index, :new, :create]
-    resources :condo_user_posts, only: [:index, :new, :create :show]
+    resources :condo_user_posts, only: [:index, :new, :create, :show]
   end
 
   root to: 'condos#index'
