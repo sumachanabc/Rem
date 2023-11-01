f31d40bd97f945c47dc397d2e88f9323529352f8
SN_sumachan <bike.cbrider@gmail.com>
2023-11-02 00:55:59 +0900

    社員側ビューに氏名と部屋番号表示追加

--- app/policies/condo_policy.rb
+++ app/policies/condo_policy.rb
@@ -13,7 +13,8 @@ class CondoPolicy < ApplicationPolicy
   end
 
   def inquiries?
-
+    # userがnilでなく、adminかmanageのロールを持っているか、物件の担当である場合にtrueを返す
+    user && (user.admin? || user.manage? || user_is_property_manager?)
   end
 
   def create?
@@ -34,6 +35,13 @@ class CondoPolicy < ApplicationPolicy
 
   def destroy?
     user.admin? || user.manage?
+  end
+
+  private
+
+  def user_is_property_manager?
+    # userとcondoが同じ外部キー（user_id）を持っている場合にtrueを返す
+    record.user_id == user.id
   end
 
 end
