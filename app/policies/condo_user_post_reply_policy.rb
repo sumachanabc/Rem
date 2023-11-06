27860aa138a3a9307bce5316f767a5e1b1ec6e60
SN_sumachan <bike.cbrider@gmail.com>
2023-11-06 22:58:49 +0900

    ポリシー変更

--- app/policies/condo_user_post_reply_policy.rb
+++ app/policies/condo_user_post_reply_policy.rb
@@ -1,7 +1,10 @@
 class CondoUserPostReplyPolicy < ApplicationPolicy
+
   def create?
+    return false if user.nil?
     user.admin? || user.manage? || user.employee? || user.id == record.condo_user_id
   end
+  
 
   def update?
     create?
