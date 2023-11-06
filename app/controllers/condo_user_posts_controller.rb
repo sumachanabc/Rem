e3bab191baba5457ea3c37dfa1616618399e55d6
SN_sumachan <bike.cbrider@gmail.com>
2023-11-06 22:06:40 +0900

    区分所有者側一覧表示修正

--- app/controllers/condo_user_posts_controller.rb
+++ app/controllers/condo_user_posts_controller.rb
@@ -6,7 +6,15 @@ class CondoUserPostsController < ApplicationController
   end
 
   def inquiries
+    @condo = Condo.find(params[:condo_id]) # この行を追加
     @condo_user_posts = CondoUserPost.where(condo_user: current_condo_user)
+                                     .includes(:condo_user_post_replies)
+                                     .sort_by { |post|
+                                       [
+                                         -post.created_at.to_i,
+                                         -post.condo_user_post_replies.count
+                                       ]
+                                     }
   end
 
   def new
