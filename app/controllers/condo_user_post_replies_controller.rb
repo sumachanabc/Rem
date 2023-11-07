6840d5a94183f2ab3a54b556e36a56e9a8a72ee1
SN_sumachan <bike.cbrider@gmail.com>
2023-11-07 22:09:04 +0900

    create記述一旦保存できるまで簡易化

--- app/controllers/condo_user_post_replies_controller.rb
+++ app/controllers/condo_user_post_replies_controller.rb
@@ -3,33 +3,22 @@ class CondoUserPostRepliesController < ApplicationController
   before_action :set_reply, only: [:update]
 
   def create
-    @reply = @condo_user_post.condo_user_post_replies.new(reply_params)
-    @reply.user = current_user # ここでログインしているユーザーを設定
-    @reply.condo_user = @condo_user_post.condo_user # ここで投稿に関連するcondo_userを設定
+    # current_user または current_condo_user が存在するか確認
+    if current_user.nil? && current_condo_user.nil?
+      redirect_to some_path, alert: 'ログインが必要です'
+      return
+    end
 
-    authorize @reply
+    # @reply を初期化し、ユーザー情報を設定
+    @reply = @condo_user_post.condo_user_post_replies.build(reply_params)
+    @reply.user = current_user if current_user
+    @reply.condo_user = current_condo_user if current_condo_user
 
     if @reply.save
-      # 画像を添付する場合
-      @reply.image.attach(params[:condo_user_post_reply][:image]) if params[:condo_user_post_reply][:image].present?
-
-      redirect_to condo_condo_user_post_path(@condo_user_post.condo, @condo_user_post), notice: 'コメントが投稿されました'
+      redirect_to [@condo_user_post.condo, @condo_user_post], notice: 'コメントが投稿されました'
     else
-      flash.now[:alert] = @reply.errors.full_messages.to_sentence
-      render 'condo_user_posts/show'
-    end
-  end
-
-  def update
-    authorize @reply
-
-    if @reply.update(reply_params)
-      # 画像を添付する場合
-      @reply.image.attach(params[:condo_user_post_reply][:image]) if params[:condo_user_post_reply][:image].present?
-
-      redirect_to condo_user_post_path(@reply.condo_user_post), notice: 'コメントが更新されました'
-    else
-      render 'condo_user_posts/show'
+      puts @reply.errors.full_messages # これでエラーメッセージを確認できます
+      redirect_to [@condo_user_post.condo, @condo_user_post], alert: 'コメントの保存に失敗しました'
     end
   end
 
@@ -44,7 +33,7 @@ class CondoUserPostRepliesController < ApplicationController
   end
 
   def reply_params
-    params.require(:condo_user_post_reply).permit(:content, :image)
+    params.require(:condo_user_post_reply).permit(:content)
   end
 
 end
