91525206ffb671ef71cc051bff4f7452702ac4d6
SN_sumachan <bike.cbrider@gmail.com>
2023-11-07 22:08:20 +0900

    一旦trueでお試し中

--- app/policies/condo_user_post_reply_policy.rb
+++ app/policies/condo_user_post_reply_policy.rb
@@ -1,8 +1,7 @@
 class CondoUserPostReplyPolicy < ApplicationPolicy
 
   def create?
-    return false if user.nil?
-    user.admin? || user.manage? || user.employee? || user.id == record.condo_user_id
+    true
   end
   
 
