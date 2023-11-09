1a7f32923a1128d22bb4811646168acca0054182
SN_sumachan <bike.cbrider@gmail.com>
2023-11-10 00:18:41 +0900

    コメント通知作成メソッド

--- app/models/condo_user_post_reply.rb
+++ app/models/condo_user_post_reply.rb
@@ -7,4 +7,23 @@ class CondoUserPostReply < ApplicationRecord
   has_one_attached :image
 
   validates :content, presence: true
+
+  after_create :create_notification_for_comment
+
+  private
+
+  def create_notification_for_comment
+    post = self.condo_user_post
+    sender = self.user || self.condo_user
+    recipient = self.user ? post.condo_user : post.condo_user.user
+
+    Notification.create!(
+      visitor_type: sender.class.name,
+      visitor_id: sender.id,
+      visited_type: recipient.class.name,
+      visited_id: recipient.id,
+      condo_user_post_reply_id: self.id,
+      action: 'comment'
+    )
+  end
 end
