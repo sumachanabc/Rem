aac5076fc28db7173e7518efb51c3c94139a380e
SN_sumachan <bike.cbrider@gmail.com>
2023-11-25 05:45:31 +0900

    document.rbにアソシエーションuser追加

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,6 @@
 class Document < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   belongs_to :condo
   has_one_attached :file
   belongs_to :document_category, class_name: 'DocumentCategory', foreign_key: 'category_id'
