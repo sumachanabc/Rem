767ee3ae5500b75548ae3be80dd52638ffcdb302
SN_sumachan <bike.cbrider@gmail.com>
2023-11-26 22:53:21 +0900

    過去実装アクティブハッシュinteger変更

--- db/migrate/20231116101528_create_documents.rb
+++ db/migrate/20231116101528_create_documents.rb
@@ -2,7 +2,7 @@ class CreateDocuments < ActiveRecord::Migration[7.0]
   def change
     create_table :documents do |t|
       t.string :title,                        null: false
-      t.string :category_id,                  null: false
+      t.integer :category_id,                 null: false
       t.references :condo, null: false, foreign_key: true
       t.references :user,  null: false, foreign_key: true
 
