0b7b29ec82b61b41c2c14b19a8e1d32c28749019
SN_sumachan <bike.cbrider@gmail.com>
2023-11-29 11:08:45 +0900

    マイグレーションファイル変更

--- db/migrate/20231125153413_create_contracts.rb
+++ db/migrate/20231125153413_create_contracts.rb
@@ -1,11 +1,12 @@
 class CreateContracts < ActiveRecord::Migration[7.0]
   def change
     create_table :contracts do |t|
-      t.integer :vehicle_type_id,   null: true
-      t.string :vehicle_model_code, null: true
       t.string :vehicle_number,     null: true
+      t.integer :vehicle_type_id,   null: false
+      t.string :vehicle_model_code, null: true
       t.date :start_date,           null: false
-      t.date :end_date
+      t.date :end_date,             null: false
+      t.references :condo,          null: false, foreign_key: true
       t.references :condo_user,     null: false, foreign_key: true
       t.references :parking,        null: false, foreign_key: true
       t.timestamps
