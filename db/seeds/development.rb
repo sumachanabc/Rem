ef436847e9684016a241bb084da15a178604ffd8
SN_sumachan <bike.cbrider@gmail.com>
2023-11-30 23:09:25 +0900

    seedファイル区画と契約追加

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -389,4 +389,76 @@ documents_to_upload.each do |doc|
   document.save!
 end
 
+# ---区画情報---
+# 各マンションに対して駐輪場区画1-10番、駐車場1,2番を全て平置きで特別カテゴリなしで生成
+condos.each do |condo|
+  # 駐輪場区画1-10番の初期データ
+  (1..10).each do |number|
+    Parking.create!(
+      condo_id: condo.id,
+      parking_number: number.to_s,
+      vehicle_type_id: 3,
+      parking_type_id: 2,
+      roof: false,
+      special_category_id: nil
+    )
+  end
+
+  # 駐車場1,2番の初期データ
+  (1..2).each do |number|
+    Parking.create!(
+      condo_id: condo.id,
+      parking_number: number.to_s,
+      vehicle_type_id: 2,
+      parking_type_id: 2,
+      roof: false,
+      special_category_id: nil
+    )
+  end
+end
+
+# ‐‐‐区画契約‐‐‐
+
+car_model_codes = ['DBA-ZC6', 'UA-NCP31', 'GH-ANH10', 'LDA-GDJ150W', 'CBA-R35']
+
+condos.each do |condo|
+
+  start_date = Date.new(2023, 4, 1)
+  end_date = Date.new(2025, 3, 31)
+
+  # 駐輪場区画1-10番の契約データ作成
+  (1..10).each do |number|
+    parking = Parking.find_by(condo_id: condo.id, parking_number: number.to_s, vehicle_type_id: 3)
+    Contract.create!(
+      vehicle_number: nil,
+      vehicle_type_id: parking.vehicle_type_id,
+      vehicle_model_code: nil,
+      start_date: start_date,
+      end_date: end_date,
+      condo_id: condo.id,
+      condo_user_id: condo.condo_users.sample.id,
+      parking_id: parking.id
+    )
+  end
+
+  # 駐車場1,2番の契約データ作成
+  (1..2).each do |number|
+    parking = Parking.find_by(condo_id: condo.id, parking_number: number.to_s, vehicle_type_id: 2)
+
+    vehicle_number = "東京 #{rand(1000..9999)}"
+    
+    vehicle_model_code = car_model_codes.sample
+
+    Contract.create!(
+      vehicle_number: vehicle_number,
+      vehicle_type_id: parking.vehicle_type_id,
+      vehicle_model_code: vehicle_model_code,
+      start_date: start_date,
+      end_date: end_date,
+      condo_id: condo.id,
+      condo_user_id: condo.condo_users.sample.id,
+      parking_id: parking.id
+    )
+  end
+end
 
